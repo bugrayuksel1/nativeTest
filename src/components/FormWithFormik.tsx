591fe82ce1d4ae9bd4fc618541925dfdba71b31c
bugrayuksel1 <oguzhanbugrayuksel@gmail.com>
2024-11-01 13:32:30 +0300

    formik'i return içinde kullanmak istemezsen daha sade hali.

--- src/components/FormWithFormik.tsx
+++ src/components/FormWithFormik.tsx
@@ -1,65 +1,61 @@
 import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 import React, {useState} from 'react';
-import {Formik} from 'formik';
+import {Formik, useFormik} from 'formik';
 
 const FormWithFormik = () => {
+  const {values, handleSubmit, handleChange} = useFormik({
+    initialValues: {
+      username: '',
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    },
+    onSubmit: values => {
+      console.log(values);
+    },
+  });
   return (
     <View style={styles.container}>
-      <Formik
-        initialValues={{
-          username: '',
-          email: '',
-          password: '',
-          passwordConfirm: '',
-        }}
-        onSubmit={values => {
-          console.log(values);
-        }}>
-        {({values, handleSubmit, handleChange}) => (
-          <>
-            <View style={styles.item}>
-              <TextInput
-                style={styles.input}
-                placeholder="username"
-                value={values.username}
-                onChangeText={handleChange('username')}
-              />
-            </View>
+      <View style={styles.item}>
+        <TextInput
+          style={styles.input}
+          placeholder="username"
+          value={values.username}
+          onChangeText={handleChange('username')}
+        />
+      </View>
 
-            <View style={styles.item}>
-              <TextInput
-                style={styles.input}
-                placeholder="e-mail"
-                autoCapitalize="none"
-                keyboardType="email-address"
-                value={values.email}
-                onChangeText={handleChange('email')}
-              />
-            </View>
-            <View style={styles.item}>
-              <TextInput
-                style={styles.input}
-                placeholder="password"
-                secureTextEntry
-                value={values.password}
-                onChangeText={handleChange('password')}
-              />
-            </View>
-            <View style={styles.item}>
-              <TextInput
-                style={styles.input}
-                placeholder="passwordConfirm"
-                secureTextEntry
-                value={values.passwordConfirm}
-                onChangeText={handleChange('passwordConfirm')}
-              />
-            </View>
-            <View style={styles.item}>
-              <Button title="Register" onPress={handleSubmit} />
-            </View>
-          </>
-        )}
-      </Formik>
+      <View style={styles.item}>
+        <TextInput
+          style={styles.input}
+          placeholder="e-mail"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          value={values.email}
+          onChangeText={handleChange('email')}
+        />
+      </View>
+      <View style={styles.item}>
+        <TextInput
+          style={styles.input}
+          placeholder="password"
+          secureTextEntry
+          value={values.password}
+          onChangeText={handleChange('password')}
+        />
+      </View>
+      <View style={styles.item}>
+        <TextInput
+          style={styles.input}
+          placeholder="passwordConfirm"
+          secureTextEntry
+          value={values.passwordConfirm}
+          onChangeText={handleChange('passwordConfirm')}
+        />
+      </View>
+      <View style={styles.item}>
+        <Button title="Register" onPress={handleSubmit} />
+      </View>
     </View>
   );
 };
